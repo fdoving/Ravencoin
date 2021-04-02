61d8af0b768c7a5e12909a2a574dd69a030a2ba0
fdov <fd21@pm.me>
2021-04-02 11:17:56 +0200

    Build: Openssl     -  Fix trailig && \ in Makefile.

--- depends/packages/openssl.mk
+++ depends/packages/openssl.mk
@@ -50,7 +50,7 @@ $(package)_config_opts_i686_mingw32=mingw
 endef
 
 define $(package)_preprocess_cmds
-  sed -i.old "/define DATE/d" util/mkbuildinf.pl && \
+  sed -i.old "/define DATE/d" util/mkbuildinf.pl
 endef
 
 define $(package)_config_cmds
