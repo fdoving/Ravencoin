9100366eb2b99978025e7c15382ae651256dedc7
fdov <fd21@pm.me>
2021-04-03 19:22:48 +0200

    Depends: Openssl correctly cross-compile for windows.

--- depends/packages/openssl.mk
+++ depends/packages/openssl.mk
@@ -44,8 +44,8 @@ $(package)_config_opts_mipsel_linux=linux-generic32
 $(package)_config_opts_mips_linux=linux-generic32
 $(package)_config_opts_powerpc_linux=linux-generic32
 $(package)_config_opts_x86_64_darwin=darwin64-x86_64-cc
-$(package)_config_opts_x86_64_mingw32=mingw64
-$(package)_config_opts_i686_mingw32=mingw
+$(package)_config_opts_x86_64_mingw32=--cross-compile-prefix=x86_64-w64-mingw64- mingw64
+$(package)_config_opts_i686_mingw32=--cross-compile-prefix=i686-w64-mingw32- mingw32
 endef
 
 define $(package)_preprocess_cmds
