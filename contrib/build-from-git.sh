2b9281bf88813409aad7ac9230e433d2f9d54c4a
fdov <fd21@pm.me>
2021-04-03 17:18:28 +0200

    Contrib: Update script

--- contrib/build-from-git.sh
+++ contrib/build-from-git.sh
@@ -2,31 +2,37 @@
 # Script is made for ubuntu, might work on debian and alike.
 # USAGE: ./build-from-git.sh <platform>
 # where platform is one of: windows, osx, linux, arm32v7
+
+WORKPRE=/build-$1
+GITDIR=Ravencoin
+WORKDIR=$WORKPRE/$GITDIR/
+rm -rf $WORKPRE
+mkdir $WORKPRE
+
 # make sure we have git
 apt update
 apt install git
 
-
 # checkout git, modify to your own usage.
+cd $WORKPRE
 git clone https://github.com/fdoving/Ravencoin.git
-cd Ravencoin
+cd $GITDIR
 git checkout fdov-update-depends
 
 # install depends from apt.
-.github/00-install-deps.sh $1
-
+$WORKDIR/.github/scripts/00-install-deps.sh $1
 
 # build or copy depends
-.github/02-copy-build-dependencies.sh $1
+$WORKDIR/.github/scripts/02-copy-build-dependencies.sh $1 $WORKDIR
 
 # setup environment
-.github/03-export-path.sh $1 `pwd`
+$WORKDIR/.github/scripts/03-export-path.sh $1 $WORKDIR
 
 # autogen
-./autogen.sh
+$WORKDIR/autogen.sh
 
 # configure build
-.github/04-configure-build.sh $1 `pwd`
+$WORKDIR/.github/scripts/04-configure-build.sh $1 $WORKDIR
 
 # build
-make -j21
+make -j22
